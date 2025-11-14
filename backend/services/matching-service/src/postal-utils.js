b11561bb268423419ead6decc0135d26ab9b9bcc
DanielSoongJiaKang <64388902+DanielSoongJiaKang@users.noreply.github.com>
2025-11-14 18:30:22 +0800

    changed postal-utils

--- backend/services/matching-service/src/postal-utils.js
+++ backend/services/matching-service/src/postal-utils.js
@@ -5,30 +5,90 @@ function getAreaFromPostalCode(postalCode) {
 
     const sector = parseInt(postal.substring(0, 2));
     const sectorMap = {
-        1: 'Raffles Place', 2: 'Tanjong Pagar', 3: 'Tiong Bahru',
-        4: 'Telok Blangah', 5: 'Pasir Panjang', 6: 'High Street',
-        7: 'Beach Road', 8: 'Little India', 9: 'Orchard',
-        10: 'Tanglin', 11: 'Novena', 12: 'Toa Payoh',
-        13: 'Macpherson', 14: 'Geylang', 15: 'Katong', 16: 'Bedok',
-        17: 'Changi', 18: 'Pasir Ris', 19: 'Serangoon', 20: 'Hougang',
-        21: 'Upper Serangoon', 22: 'Sengkang', 23: 'Punggol',
-        24: 'Lim Chu Kang', 25: 'Kranji', 26: 'Upper Thomson', 27: 'Yishun',
-        28: 'Seletar', 29: 'Sembawang', 30: 'Mandai', 31: 'Toa Payoh',
-        32: 'Toa Payoh', 33: 'Toa Payoh', 34: 'Springleaf', 35: 'Woodlands',
-        36: 'Woodlands', 37: 'Woodlands', 38: 'Woodlands',
-        39: 'Woodlands', 40: 'Choa Chu Kang', 41: 'Choa Chu Kang',
-        42: 'Bukit Batok', 43: 'Bukit Batok', 44: 'Bukit Batok',
-        45: 'Bukit Batok', 46: 'Clementi', 47: 'Clementi',
-        48: 'Jurong East', 49: 'Jurong East', 50: 'Jurong West',
-        51: 'Jurong West', 52: 'Jurong West', 53: 'Bukit Merah',
-        54: 'Bukit Merah', 55: 'Queenstown', 56: 'Ang Mo Kio',
-        57: 'Ang Mo Kio', 58: 'Bishan', 59: 'Serangoon', 60: 'Bishan',
-        61: 'Upper Bukit Timah', 62: 'Alexandra', 63: 'Bukit Merah',
-        64: 'Harbourfront', 65: 'Buona Vista', 66: 'Pasir Panjang',
-        67: 'High Street', 68: 'Beach Road', 69: 'Rochor', 70: 'Kallang',
-        71: 'Geylang', 72: 'Paya Lebar', 73: 'Marine Parade', 74: 'Bedok',
-        75: 'Simei', 76: 'Tampines', 77: 'Pasir Ris', 78: 'Pasir Ris',
-        79: 'Tampines', 80: 'Tampines', 81: 'Changi', 82: 'Changi'
+        // District 01 - Raffles Place, Cecil, Marina, People's Park
+        1: 'Raffles Place', 2: 'Raffles Place', 3: 'Raffles Place',
+        4: 'Raffles Place', 5: 'Raffles Place', 6: 'Raffles Place',
+
+        // District 02 - Anson, Tanjong Pagar
+        7: 'Tanjong Pagar', 8: 'Tanjong Pagar',
+
+        // District 03 - Queenstown, Tiong Bahru
+        14: 'Queenstown', 15: 'Queenstown', 16: 'Queenstown',
+
+        // District 04 - Telok Blangah, Harbourfront
+        9: 'Telok Blangah', 10: 'Harbourfront',
+
+        // District 05 - Pasir Panjang, Hong Leong Garden, Clementi New Town
+        11: 'Pasir Panjang', 12: 'Clementi', 13: 'Clementi',
+
+        // District 06 - High Street
+        17: 'High Street',
+
+        // District 07 - Middle Road, Golden Mile, Beach Road
+        18: 'Beach Road', 19: 'Beach Road',
+
+        // District 08 - Little India
+        20: 'Little India', 21: 'Little India',
+
+        // District 09 - Orchard, Cairnhill, River Valley
+        22: 'Orchard', 23: 'Orchard',
+
+        // District 10 - Ardmore, Bukit Timah, Holland Road, Tanglin
+        24: 'Tanglin', 25: 'Tanglin', 26: 'Tanglin', 27: 'Tanglin',
+
+        // District 11 - Watten Estate, Novena, Thomson
+        28: 'Novena', 29: 'Novena', 30: 'Novena',
+
+        // District 12 - Balestier, Toa Payoh
+        31: 'Toa Payoh', 32: 'Toa Payoh', 33: 'Toa Payoh',
+
+        // District 13 - Macpherson, Braddell, Potong Pasir
+        34: 'Macpherson', 35: 'Macpherson', 36: 'Macpherson', 37: 'Macpherson',
+
+        // District 14 - Geylang, Eunos
+        38: 'Geylang', 39: 'Geylang', 40: 'Geylang', 41: 'Geylang',
+
+        // District 15 - Katong, Joo Chiat, Amber Road
+        42: 'Katong', 43: 'Katong', 44: 'Katong', 45: 'Katong',
+
+        // District 16 - Bedok, Upper East Coast
+        46: 'Bedok', 47: 'Bedok', 48: 'Bedok',
+
+        // District 17 - Loyang, Changi
+        49: 'Changi', 50: 'Changi', 81: 'Changi',
+
+        // District 18 - Simei, Tampines, Pasir Ris
+        51: 'Pasir Ris', 52: 'Tampines',
+
+        // District 19 - Serangoon Garden, Hougang, Punggol, Sengkang
+        53: 'Hougang', 54: 'Sengkang', 55: 'Hougang', 82: 'Punggol', 83: 'Punggol',
+
+        // District 20 - Bishan, Ang Mo Kio
+        56: 'Ang Mo Kio', 57: 'Ang Mo Kio',
+
+        // District 21 - Upper Bukit Timah, Clementi Park, Ulu Pandan
+        58: 'Upper Bukit Timah', 59: 'Upper Bukit Timah',
+
+        // District 22 - Jurong
+        60: 'Jurong', 61: 'Jurong', 62: 'Jurong', 63: 'Jurong', 64: 'Jurong',
+
+        // District 23 - Hillview, Dairy Farm, Bukit Panjang, Choa Chu Kang
+        65: 'Bukit Panjang', 66: 'Bukit Panjang', 67: 'Choa Chu Kang', 68: 'Choa Chu Kang',
+
+        // District 24 - Lim Chu Kang, Tengah
+        69: 'Lim Chu Kang', 70: 'Lim Chu Kang', 71: 'Lim Chu Kang',
+
+        // District 25 - Admiralty, Woodlands
+        72: 'Woodlands', 73: 'Woodlands',
+
+        // District 26 - Upper Thomson, Springleaf
+        77: 'Upper Thomson', 78: 'Upper Thomson',
+
+        // District 27 - Yishun, Sembawang
+        75: 'Yishun', 76: 'Yishun',
+
+        // District 28 - Seletar
+        79: 'Seletar', 80: 'Seletar'
     };
 
     return sectorMap[sector] || null;
